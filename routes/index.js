fae8dfb86fedb21807731ef86a9822656e20322b
Milad Imen <mimen@ucsd.edu>
2016-03-30 13:09:55 -0700

    starting auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/home/', function(req, res, next) {
+  res.render('home');
 });
 
 module.exports = router;
