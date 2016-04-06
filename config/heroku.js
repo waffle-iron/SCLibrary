fad154d10d79c9793106ea89f6f7894f10934c1e
mimen <miladmaaan@gmail.com>
2016-04-05 20:17:49 -0700

    removed config files

--- config/heroku.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	base_url: 'http://sc-library.herokuapp.com',
-	auth: { client_id: 'b784f5490df1c16b90fe1fc07d5b619d',
-			client_secret: '5fd44b5e8554a0b22f0355f088033052',
-			redirect_uri: 'http://sc-library.herokuapp.com/home'
-		  }
-}
-
-module.exports = require('./defaults')(config);
