ad562b79353cbaea3fcd0b55104f389deb6f6221
Milad Imen <mimen@ucsd.edu>
2016-04-05 20:17:49 -0700

    removed config files

--- config/defaults.js
+++
@@ -1,9 +0,0 @@
-var _ = require('lodash');
-
-var defaults = {
-
-}
-
-module.exports = function(config){
-    return _.defaultsDeep(config, defaults);
-};
--- config/dev.js
+++
@@ -1,5 +0,0 @@
-var config = {
-	
-}
-
-module.exports = require('./defaults')(config);
