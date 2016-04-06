54a7575f30bdc6b580670d2d5ef6907b7247d5a8
mimen <miladmaaan@gmail.com>
2016-04-06 14:55:21 -0700

    turned recursive addCollection function into a while loop

--- client/database.js
+++ client/database.js
@@ -48,67 +48,55 @@ function addUser(user, done){
 
 function addCollection(user, collection, done){
     console.log("adding collection to database");
-    addTracks(user, collection, 0, done);
-}
+    var index = 0;
+    while (index < collection.length - 1){
+        track = collection[index];
+        if (track.kind == 'track'){
+            var scquery = 'MATCH (user:Channel {name: {name}}) ' + 
+                        'MERGE (t:Track { name: {title}, genre: {genre}, duration: {duration}, scid: {tid}, ' +
+                        'url: {url}, tag_list: {tag_list}, created_at: {created_at}, ';
+            if (track.purchase_url != null)
+                scquery += 'purchase_url: {purchase_url}, ';
+            if (track.artwork_url != null)
+                scquery += 'artwork_url: {artwork_url}, ';
+            scquery = scquery + 'waveform_url: {waveform_url} }) ' +
+                        'MERGE (c:Channel { name: {channel} }) ' + 
+                        'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
+                        'CREATE (user)-[r1:LIKES_TRACK]->(t) ' +
+                        'CREATE (c)-[r2:UPLOADED]->(t) ' +
+                        'RETURN user, r1, c, r2, t';
 
-function addTracks(user, collection, index, done){
-    track = collection[index];
-    if (track.kind == 'track'){
-        var scquery = 'MATCH (user:Channel {name: {name}}) ' + 
-                    'MERGE (t:Track { name: {title}, genre: {genre}, duration: {duration}, scid: {tid}, ' +
-                    'url: {url}, tag_list: {tag_list}, created_at: {created_at}, ';
-        if (track.purchase_url != null)
-            scquery += 'purchase_url: {purchase_url}, ';
-        if (track.artwork_url != null)
-            scquery += 'artwork_url: {artwork_url}, ';
-        scquery = scquery + 'waveform_url: {waveform_url} }) ' +
-                    'MERGE (c:Channel { name: {channel} }) ' + 
-                    'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
-                    'CREATE (user)-[r1:LIKES_TRACK]->(t) ' +
-                    'CREATE (c)-[r2:UPLOADED]->(t) ' +
-                    'RETURN user, r1, c, r2, t';
+            db.cypher({ 
+                query: scquery,
+                params: {
+                    name: user.username,
+                    title: track.title,
+                    duration: track.duration,
+                    genre: track.genre,
+                    tid: track.id,
+                    url: track.permalink_url,
+                    purchase_url: track.purchase_url,
+                    tag_list: track.tag_list,
+                    artwork_url: track.artwork_url,
+                    created_at: track.created_at,
+                    waveform_url: track.waveform_url,
+                    channel: track.user.username,
+                    channel_url: track.user.permalink_url,
+                    avatar_url: track.user.avatar_url,
+                    uid: track.user.id
+                },
+            }, function(err, results){
+                console.log(results);
+            });
 
-        db.cypher({ 
-            query: scquery,
-            params: {
-                name: user.username,
-                title: track.title,
-                duration: track.duration,
-                genre: track.genre,
-                tid: track.id,
-                url: track.permalink_url,
-                purchase_url: track.purchase_url,
-                tag_list: track.tag_list,
-                artwork_url: track.artwork_url,
-                created_at: track.created_at,
-                waveform_url: track.waveform_url,
-                channel: track.user.username,
-                channel_url: track.user.permalink_url,
-                avatar_url: track.user.avatar_url,
-                uid: track.user.id
-            },
-        }, function(err, results){
-            index++;
-            console.log(results);
-            if (index < collection.length - 1){
-                addTracks(user, collection, index, done);
-            }
-            else { 
-                // And forward to success page
-                done();
-            }
-        });
-
-    } else {
-        index++;
-        console.log("attempted to add playlist");
-        if (index < collection.length - 1)
-            addTracks(user, collection, index, done);
-        else { 
-            // And forward to success page
-            done();
+        } 
+        else {
+            console.log("attempted to add playlist");
         }
+        index++;
     }
+
+    done();
 }
 
 
