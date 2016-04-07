07d0e213753833e5e2de658e28252bdf9af0313e
mimen <miladmaaan@gmail.com>
2016-04-07 14:35:33 -0700

    removed API for now

--- routes/api/collections/add.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var requestify = require('requestify');
-var config = require('../../../config.js');
-//var Q = require('q');
-var soundcloud = require('../../../client/soundcloud');
-var router = express.Router();
-
-module.exports = router;
