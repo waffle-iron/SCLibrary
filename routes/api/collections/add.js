e48ca99149af253a47c728962bad6023c420391e
Milad Imen <mimen@ucsd.edu>
2016-04-06 21:53:51 -0700

    cleaned out unnecessary files

---
+++ routes/api/collections/add.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var requestify = require('requestify');
+var config = require('../../../config.js');
+//var Q = require('q');
+var soundcloud = require('../../../client/soundcloud');
+var router = express.Router();
+
+module.exports = router;
---
+++ routes/api/users/add.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var requestify = require('requestify');
+var config = require('../../../config.js');
+//var Q = require('q');
+var soundcloud = require('../../../client/soundcloud');
+var router = express.Router();
+
+
+
+
+
+module.exports = router;
