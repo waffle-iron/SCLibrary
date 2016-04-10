29c10c26c6f7a9c2d0933fabccf306facff9341c
mimen <miladmaaan@gmail.com>
2016-04-10 13:25:15 -0700

    fixed requiresUser route

--- routes/requiresUser.js
+++ routes/requiresUser.js
@@ -1,8 +1,8 @@
 
 function requiresUser(req, res, next) {
-    if (!req.user) {
+    if (!req.session.user) {
         console.log("no user logged in");
-        req.flash('error', "You must be logged in to continue");
+        //req.flash('error', "You must be logged in to continue");
         res.redirect('/');
     } else {
         console.log("logged in");
