0d721d3edef985adde85f1559f270a8bdc623637
Milad Imen <mimen@ucsd.edu>
2016-04-09 23:48:14 -0700

    added checks to make sure user is logged in

---
+++ routes/requiresUser.js
@@ -0,0 +1,13 @@
+
+function requiresUser(req, res, next) {
+    if (!req.user) {
+        console.log("no user logged in");
+        req.flash('error', "You must be logged in to continue");
+        res.redirect('/');
+    } else {
+        console.log("logged in");
+        next();
+    }
+}
+
+module.exports = requiresUser;
