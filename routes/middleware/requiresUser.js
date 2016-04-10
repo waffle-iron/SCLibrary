8e41e3ee8ecf1c10aa84495227830195f3e4d130
mimen <miladmaaan@gmail.com>
2016-04-10 15:12:38 -0700

    created middlware for ensuring user is logged out and reorganized files

--- routes/middleware/requiresUser.js
+++ routes/middleware/requiresUser.js
@@ -1,8 +1,8 @@
 
 function requiresUser(req, res, next) {
     if (!req.session.user) {
-        console.log("no user logged in");
-        //req.flash('error', "You must be logged in to continue");
+        console.log("logged out");
+        req.flash('error', "You must be logged in to continue");
         res.redirect('/');
     } else {
         console.log("logged in");
