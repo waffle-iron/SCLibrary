2578f323df801d329f0c69effcb1b585ed678ad8
mimen <miladmaaan@gmail.com>
2016-04-10 11:20:40 -0700

    library now works, can sort / search your library

--- public/js/lib/library.js
+++ public/js/lib/library.js
@@ -1,11 +1,14 @@
 console.log("loaded js");
 
+
+
+
+
 angular.module("Library", [])
     .factory("libraryLoader", ["$http", function ($http) {
         function load(done) {
             $http.get('http://localhost:3000/api/mycollection')
                 .success(function(data, status, headers, config){
-                    console.log (this);
                     done(null, data);
                 })
                 .error(function(data, status, headers, config){
@@ -26,6 +29,12 @@ angular.module("Library", [])
             controller: ["libraryLoader", "$q", '$http', function (libraryLoader, $q, $http) {
                 console.log("in directive controller");
                 var ctlr = this;
+                ctlr.convertTime = function(time){
+                    var min_sec = time / 1000 / 60;
+                    var minutes = Math.floor(min_sec);
+                    var seconds = ("00" + Math.floor((min_sec % 1) * 60)).slice(-2);
+                    return minutes + ":" + seconds;
+                }
                 libraryLoader.load(function (err, result) {
                     if (err) {
                         ctlr.isLoaded = false;
@@ -34,43 +43,22 @@ angular.module("Library", [])
                         ctlr.data = result;
                         console.log(ctlr.data);
                         ctlr.isLoaded = true;
+                        setTimeout(function() { createList(); }, 1000);
+
+
                     }
                 });
             }],
             controllerAs: "ctlr"
         };
-    }])
-    .controller("LibraryController", ["$scope", function ($scope) {
-        var ctlr = this;
-/*
-        ctlr.getOrder = function (order) {
-            window.open('/api/orders/' + order.storeId + '/' + order.id)
-        };
-
-        ctlr.getSnapshotInterchange = function (line_item) {
-            window.open('/api/snapshot/export/lineitem/' + line_item.snapshot_id);
-        };
-
-        ctlr.getAsset = function (line_item, item, asset) {
-            window.open('/api/snapshot/preview/' + line_item.snapshot_id + '/asset/' + item + '/' + asset);
-        };
-
-        ctlr.getVariableDisplayName = function(variable){
-            var name = "";
-            variable.forEach(function(pair){
-                if (pair.key == "prototype"){
-                    name = pair.value.displayName;
-                }
-            });
-            return name;
-        };
-
-        ctlr.previewSvg = function(svg){
-            var x = window.open();
-            var open = '<svg id="scale-box" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 345.3971354106 234.94596353754002" preserveAspectRatio="xMinYMin meet">'
-            var close = '</svg>';
-            console.log(open + svg + close);
-            x.document.write(open + svg + close);
-        }
-*/
     }]);
+
+
+var options = {
+    valueNames: ['name', 'genre', 'duration']
+};
+
+function createList(){
+    console.log("cretae list");
+    new List('library', options);
+}
