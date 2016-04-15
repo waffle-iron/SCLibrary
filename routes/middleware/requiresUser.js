9f5feefdf935c855f91f6540878054bad3c585e9
Milad Imen <mimen@ucsd.edu>
2016-04-14 19:44:09 -0700

    cleanup

--- routes/middleware/requiresUser.js
+++ routes/middleware/requiresUser.js
@@ -2,7 +2,7 @@
 function requiresUser(req, res, next) {
     if (!req.session.user) {
         console.log("logged out");
-        req.flash('error', "You must be logged in to continue");
+        //req.flash('error', "You must be logged in to continue");
         res.redirect('/');
     } else {
         console.log("logged in");
