ed6f695bb959166863db510bb4810ca4cebf59aa
Milad Imen <mimen@ucsd.edu>
2016-04-14 19:44:03 -0700

    added error handling to playlist api

--- routes/api/playlist.js
+++ routes/api/playlist.js
@@ -4,41 +4,56 @@ var db = require('../../client/database');
 
 /* GET API/playlist index page. */
 router.get('/', function(req, res, next) {
-	res.json({ error: 'no id' });
+	res.json({ error: 'you must supply a playlist id' });
 });
 
 /* GET playlist */
 router.get('/:id', function(req, res, next) {
-	db.getPlaylist(req.params.id, function(collection){
-		res.json(collection);
+	db.getPlaylist(req.params.id, function(collection, error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json(collection);
 	});
 });
 
 /* POST new playlist */
 router.post('/', function(req, res, next){
-	db.createPlaylist(req.body.name, req.body.uid, function(){
-		res.json();
+	db.createPlaylist(req.body.name, req.body.uid, function(error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json({"success":"playlist created"});
 	});
 });
 
 /* DELETE playlist */
 router.post('/:id', function(req, res, next){
-	db.deletePlaylist(req.params.id, function(){
-		res.json();
+	db.deletePlaylist(req.params.id, function(error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json({"success":"playlist deleted"});
 	});
 });
 
 /* POST track to playlist */
 router.post('/:pid/add/:tid', function(req, res, next){
-	db.addTrackToPlaylist(req.params.tid, req.params.pid, function(){
-		res.json();
+	db.addTrackToPlaylist(req.params.tid, req.params.pid, function(error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json({"success":"track added to playlist"});
 	});
 });
 
 /* DELETE track frpm playlist */
 router.post('/:pid/remove/:tid', function(req, res, next){
-	db.addTrackToPlaylist(req.params.tid, req.params.pid, function(){
-		res.json();
+	db.addTrackToPlaylist(req.params.tid, req.params.pid, function(error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json({"success":"track removed from playlist"});
 	});
 });
 
