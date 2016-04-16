c1842e79f42d581b85ed5fcd9f69445a66e2e588
Milad Imen <mimen@ucsd.edu>
2016-04-16 16:18:01 -0700

    refactored database client into multiple files to reduce file lengths

---
+++ client/database/collections.js
@@ -0,0 +1,152 @@
+module.exports = function(db){
+
+
+    var module = {};
+
+    // Given a collection of tracks and a user, create [:LIKES] relationships
+    // between each track and the user if they do not already exist, while adding
+    // tracks to the database that do not yet exist.
+    module.addCollection = function(user, collection, done){
+        console.log("adding collection to database");
+        addTracks(user, collection, 0, done);
+    };
+
+    // Recursive helper function for addCollection.
+    function addTracks(user, collection, index, done){
+        track = collection[index];
+        if (track.kind == 'track'){
+            
+            module.checkExistence(user, track, function(found, error){
+                if (error)
+                    done(error);
+                //user to track relationship already exists in database; done.
+                if (found) 
+                    done();
+                //otherwise relationship does not yet exist; create it.
+                else {
+                    var query = 'MATCH (user:Channel {name: {name}}) ' + 
+                                'MERGE (t:Track { name: {title}, duration: {duration}, scid: {tid}, ' +
+                                'url: {url}, tag_list: {tag_list}, created_at: {created_at}, ';
+                    if(track.genre != null)
+                        query += 'genre: {genre}, ';
+                    if (track.purchase_url != null)
+                        query += 'purchase_url: {purchase_url}, ';
+                    if (track.artwork_url != null)
+                        query += 'artwork_url: {artwork_url}, ';
+                    query = query + 'waveform_url: {waveform_url} }) ' +
+                                'MERGE (c:Channel { name: {channel} }) ' + 
+                                'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
+                                'CREATE (user)-[r1:LIKES_TRACK]->(t) ' +
+                                'CREATE (c)-[r2:UPLOADED]->(t) ' +
+                                'RETURN user, r1, c, r2, t';
+
+                    db.cypher({ 
+                        query: query,
+                        params: {
+                            name: user.username,
+                            title: track.title,
+                            duration: track.duration,
+                            genre: track.genre,
+                            tid: track.id,
+                            url: track.permalink_url,
+                            purchase_url: track.purchase_url,
+                            tag_list: track.tag_list,
+                            artwork_url: track.artwork_url,
+                            created_at: track.created_at,
+                            waveform_url: track.waveform_url,
+                            channel: track.user.username,
+                            channel_url: track.user.permalink_url,
+                            avatar_url: track.user.avatar_url,
+                            uid: track.user.id
+                        },
+                    }, function(error, results){
+                        if (error){
+                            done(error);
+                        }
+                        else {
+                            console.log("Track added!");
+                            index++;
+                            if (index < collection.length - 1) 
+                                addTracks(user, collection, index, done);
+                            else
+                                done();
+                        }
+                    });
+
+                }
+
+            })
+
+        } 
+        else {
+            console.log("attempted to add playlist");
+            //TODO: Add tracks from this playlist 
+            index++;
+            if (index < collection.length - 1) 
+                addTracks(user, collection, index, done);
+            else
+                done();
+        }
+    }
+
+
+    // Check for the existence of a [:LIKES] relationship between a channel and a track. 
+    // Return true if it exists, false if it does not. 
+    module.checkExistence = function(user, track, done){
+
+        db.cypher({ 
+            query: 'MATCH (user:Channel {scuid: {scuid} }), (t:Track {scid: {scid} } ), (user)-[:LIKES_TRACK]->(t) return user, t',
+            params: {
+                scuid: user.id,
+                scid: track.id
+            },
+
+        }, function(error, results){
+            if (error){
+                done(null, error);
+            }
+            else {      
+                //console.log(results);
+                // If no match, create an entry for the user
+                if (results.length == 0) {
+                    console.log("No relationship found.");
+                    done(false);
+                    //TODO: Update access token
+                }
+                else {
+                    console.log("Relationship already exists.");
+                    done(true);
+                }
+            }
+        });
+    };
+
+    // Given a user, find and return their entire collection of songs, along with the channels
+    // that uploaded them.
+    module.getCollection = function(user, done){
+        db.cypher({ 
+            query: 'MATCH (currUser:Channel { scuid: {scuid}}),(currUser)-[:LIKES_TRACK]->(t)'
+                    + '<-[:UPLOADED]-(c) RETURN t, c',
+            params: {
+                scuid: user.id
+            },
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(null, error);
+            }
+            else {
+                //console.log(results);
+                // No collection found for the user
+                if (results.length == 0) {
+                    console.log("no collection found for this user");
+                // If match found, do nothing
+                } else {
+
+                }
+                done(results);
+            }
+        });
+    };
+    return module;
+}
---
+++ client/database/playlists.js
@@ -0,0 +1,134 @@
+module.exports = function(db){
+
+    var module = {};
+
+    // Given a playlist name and a user id, create a playlist and assign 
+    // ownership to the user.
+    module.createPlaylist = function(name, uid, done){
+        //TODO: Make sure playlists are not created w/ duplicate names
+        db.cypher({ 
+            query: "MATCH (user:Channel {scuid: {uid} }) " + 
+                   "CREATE (playlist:Playlist {name: {playlist_name} })<-[r:OWNS]-(user) " +
+                   "RETURN user, r, playlist",
+            params: {
+                playlist_name: name,
+                uid: uid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });
+    }
+
+    // Given a playlist id, remove that playlist from the database.
+    module.deletePlaylist = function(pid, done){
+        db.cypher({ 
+            query: "MATCH (playlist:Playlist) WHERE id(playlist) = {pid} DETACH DELETE playlist",
+            params: {
+                pid: pid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });    
+    }
+
+    // Given a track id and a playlist id, create a playlist contains track relationship.
+    module.addTrackToPlaylist = function(tid, pid, done){
+        db.cypher({ 
+            query: "MATCH (track:Track {scid: {tid} }), (playlist:Playlist) " + 
+                   "WHERE id(playlist) = {pid} " +
+                   "CREATE (playlist)-[r:CONTAINS]->(track) " +
+                   "RETURN playlist, r, track",
+            params: {
+                tid: tid,
+                pid: pid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });
+    }
+
+    // Given a track id and a playlist id, remove any playlist contains track relationship between them.
+    module.removeTrackFromPlaylist = function(tid, pid, done){
+        db.cypher({ 
+            query: "MATCH (Track {scid: {tid} })<-[r:CONTAINS]-(playlist:Playlist) " + 
+                   "WHERE id(playlist) = 7108 " +
+                   "DELETE r",
+            params: {
+                tid: tid,
+                pid: pid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });}
+
+    // Given a playlist id, return the list of all tracks contained by the playlist.
+    module.getPlaylist = function(pid, done){
+        db.cypher({ 
+            query: "MATCH (playlist:Playlist)-[:CONTAINS]->(track:Track) " +
+                   "WHERE id(playlist) = {pid}" +
+                   "RETURN track",
+            params: {
+                pid: pid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });
+    }
+
+    // Given a user, return the list of all playlists owned by the user.
+    module.getPlaylists = function(uid, done){
+        db.cypher({ 
+            query: "MATCH (Channel {scuid: {uid} })-[:OWNS]->(playlist:Playlist) " +
+                   "RETURN playlist",
+            params: {
+                pid: pid
+            }
+        }, function(error, results){
+            if (error){
+                console.log(error);
+                done(error);
+            }
+            else {      
+                console.log(results);
+                done();
+            }
+        });
+    }
+
+    return module;
+}
