75b1945cb333b6b85a4d4bbd2d45baad8033beb0
mimen <miladmaaan@gmail.com>
2016-04-16 16:18:01 -0700

    refactored database client into multiple files to reduce file lengths

---
+++ client/database/users.js
@@ -0,0 +1,76 @@
+module.exports = function(db){
+
+    var module = {};
+
+    // Add a user to the database if they do not already exist.
+    module.addUser = function(user, done){
+        // Search the database to find a match
+        db.cypher({ 
+            query: 'MATCH (user:Channel {scuid: {scuid}}) RETURN user',
+            params: {
+                scuid: user.id
+            },
+        }, function(error, results){
+            if (error){
+                done(error);
+            }
+            // If no match, create an entry for the user
+            if (results.length == 0) {
+                console.log('User not found. Going to create user ' + user.username + '.');
+                // Create an entry for the user
+                db.cypher({ 
+                    query: 'CREATE (user:Channel {name: {name}, scuid: {scuid}, permalink: {permalink}, '
+                            + 'avatar_url: {avatar_url}, country: {country} }) RETURN user',
+                    params: {
+                        name: user.username,
+                        scuid: user.id,
+                        permalink: user.permalink,
+                        avatar_url: user.avatar_url,
+                        country: user.country
+                    },
+                }, function(error, results){
+                    if (error){
+                        done(error);
+                    } else {
+                        // User was added to the database.
+                        done();
+                    }
+                });
+                
+            // If match found, do nothing
+            } else {
+                console.log("User already in database");
+                done();
+            }
+        });
+    }
+
+    // Find a user from the database given their scuid.
+    module.getUser = function(scuid, done){
+        db.cypher({ 
+            query: 'MATCH (user:Channel { scuid: {scuid} }) RETURN user',
+            params: {
+                scuid: scuid
+            },
+        }, function(error, results){
+            if (error){
+                done(null, error);
+            }
+            else {      
+                // If no match, create an entry for the user
+                if (results.length == 0) {
+                    console.log("No user found.");
+                    done(null);
+                    //TODO: Update access token
+                }
+                else {
+                    console.log("User was found.");
+                    done(results);
+                }
+            }
+        });
+    }
+
+    return module;
+
+}
