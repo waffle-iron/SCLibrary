9461616b1aacf3395b0609172052eab0fe1cffa7
mimen <miladmaaan@gmail.com>
2016-04-16 22:05:26 -0700

    added jquery-ui for draggable and droppable libraries

--- public/js/lib/library.js
+++ public/js/lib/library.js
@@ -1,149 +1,154 @@
 var queue = new Queue();
 //how to use: http://code.stephenmorley.org/javascript/queues/
 
-angular.module("Library", [])
-    // HTTP loader for API requests
-    .factory("httpLoader", ["$http", function ($http) {
-        function load(href, done) {
-            $http.get(href)
-                .success(function(data, status, headers, config){
-                    done(null, data);
-                })
-                .error(function(data, status, headers, config){
-                    console.log(status);
-                    var err = new Error(data);
-                    err.code = status;
-                    done(err);
-                });
-        }
-        return {
-            load: load
-        };
-    }])
-    // Library directive - html Element
-    .directive("library", [function (){
-        return {
-            restrict: 'E',
-            templateUrl: 'http://localhost:3000/views/library.html',
-            controller: ["httpLoader", "$q", '$http', function (httpLoader, $q, $http) {
-                var ctlr = this;
+var app = angular.module("Library", []);
 
-                // Variables used for sort and search functionality
-                ctlr.sortType = '';
-                ctlr.sortReverse = false;
-                ctlr.searchTerm = '';
 
-                // Update sort variables
-                ctlr.updateSort = function(sortBy){
-                    if (ctlr.sortType == sortBy)
-                        ctlr.sortReverse = !ctlr.sortReverse; 
-                    else
-                        ctlr.sortReverse = false;
-                    ctlr.sortType = sortBy;
-                }
+// HTTP loader for API requests
+app.factory("httpLoader", ["$http", function ($http) {
+    function load(href, done) {
+        $http.get(href)
+            .success(function(data, status, headers, config){
+                done(null, data);
+            })
+            .error(function(data, status, headers, config){
+                console.log(status);
+                var err = new Error(data);
+                err.code = status;
+                done(err);
+            });
+    }
+    return {
+        load: load
+    };
+}]);
 
-                // Convert time from ms to MM:SS
-                ctlr.convertTime = function(time){
-                    var min_sec = time / 1000 / 60;
-                    var minutes = Math.floor(min_sec);
-                    var seconds = ("00" + Math.floor((min_sec % 1) * 60)).slice(-2);
-                    return minutes + ":" + seconds;
-                }
+// Library directive - html Element
+app.directive("library", [function (){
+    return {
+        restrict: 'E',
+        templateUrl: 'http://localhost:3000/views/library.html',
+        controller: ["httpLoader", "$q", '$http', '$timeout', function (httpLoader, $q, $http, $timeout) {
+            var ctlr = this;
 
-                // Format date string
-                ctlr.formatDate = function(date){
-                    return date.substring(0, 10);
-                }
+            // Variables used for sort and search functionality
+            ctlr.sortType = '';
+            ctlr.sortReverse = false;
+            ctlr.searchTerm = '';
 
-                // Send a song to the player and save the next 20 songs for an autoplay queue
-                ctlr.playSong = function(track, element){
+            // Update sort variables
+            ctlr.updateSort = function(sortBy){
+                if (ctlr.sortType == sortBy)
+                    ctlr.sortReverse = !ctlr.sortReverse; 
+                else
+                    ctlr.sortReverse = false;
+                ctlr.sortType = sortBy;
+            }
 
-                    queue = new Queue();
-                    var i = 0;
-                    while (element.$$nextSibling && i < 20){
-                        var properties = element.$$nextSibling.track.t.properties;
-                        var options = { 
-                            scid: properties.id,
-                            duration: properties.duration,
-                            artwork_url: properties.artwork_url,
-                            waveform_url: properties.waveform_url
-                        }
-                        queue.enqueue(options);
-                        element = element.$$nextSibling;
-                        i++;
+            // Convert time from ms to MM:SS
+            ctlr.convertTime = function(time){
+                var min_sec = time / 1000 / 60;
+                var minutes = Math.floor(min_sec);
+                var seconds = ("00" + Math.floor((min_sec % 1) * 60)).slice(-2);
+                return minutes + ":" + seconds;
+            }
+
+            // Format date string
+            ctlr.formatDate = function(date){
+                return date.substring(0, 10);
+            }
+
+            // Send a song to the player and save the next 20 songs for an autoplay queue
+            ctlr.playSong = function(track, element){
+
+                queue = new Queue();
+                var i = 0;
+                while (element.$$nextSibling && i < 20){
+                    var properties = element.$$nextSibling.track.t.properties;
+                    var options = { 
+                        scid: properties.id,
+                        duration: properties.duration,
+                        artwork_url: properties.artwork_url,
+                        waveform_url: properties.waveform_url
                     }
-
-                    var properties = track.t.properties;
-                    loadSong(properties.scid, properties.duration, properties.artwork_url, properties.waveform_url);
+                    queue.enqueue(options);
+                    element = element.$$nextSibling;
+                    i++;
                 }
 
-                ctlr.createPlaylist = function(){
-                    var url = 'http://localhost:3000/api/playlists/';
-                    var data = {
-                        name: ctlr.playlistInput,
-                        uid: loggedinuser.id
-                    }
-                    $http.post(url, data).then(function(response){
-                        console.log(response);
-                        ctlr.loadPlaylists();
-                    }, function(error){
-                        console.log(error);
-                    }); 
+                var properties = track.t.properties;
+                loadSong(properties.scid, properties.duration, properties.artwork_url, properties.waveform_url);
+            }
 
-                    $('.playlistForm').hide();
+            // Add a playlist to the database and hide the new playlist form
+            ctlr.createPlaylist = function(){
+                var url = 'http://localhost:3000/api/playlists/';
+                var data = {
+                    name: ctlr.playlistInput,
+                    uid: loggedinuser.id
                 }
-
-                // Update the view with tracks from the selected playlist.
-                ctlr.loadPlaylist = function(playlist){
-                    var url = 'http://localhost:3000/api/playlists/' + playlist.p._id;
-                    httpLoader.load(url, function(err, result){
-                        if (err)
-                            console.log(err);
-                        else {
-                            ctlr.display = result;
-                        }
-                    })
-                }
-
-                // Update the view with the user's collection
-                ctlr.displaySongs = function(){
-                    ctlr.display = ctlr.collection;
-                }
-
-                // Populate the list of songs
-                ctlr.loadLibrary = function(){
-                    httpLoader.load('http://localhost:3000/api/mycollection', function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            ctlr.collection = result;
-                            ctlr.display = result;
-                        }
-                    });
-                }
-
-                // Populate the list of playlists
-                ctlr.loadPlaylists = function(){
-                    httpLoader.load('http://localhost:3000/api/myplaylists', function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            ctlr.playlists = result;
-                        }
-                    });
-                }
-
-                ctlr.loadLibrary();
-                ctlr.loadPlaylists();
+                $http.post(url, data).then(function(response){
+                    console.log(response);
+                    ctlr.loadPlaylists();
+                }, function(error){
+                    console.log(error);
+                }); 
 
                 $('.playlistForm').hide();
-                $('.addPlaylist').click(function(){
-                    $('.playlistForm').show();
-                });
+            }
 
-            }],
-            controllerAs: "ctlr"
-        };
-    }]);
+
+            // Update the view with tracks from the selected playlist.
+            ctlr.loadPlaylist = function(playlist){
+                var url = 'http://localhost:3000/api/playlists/' + playlist.p._id;
+                httpLoader.load(url, function(err, result){
+                    if (err)
+                        console.log(err);
+                    else {
+                        ctlr.display = result;
+                    }
+                })
+            }
+
+            // Update the view with the user's collection
+            ctlr.displaySongs = function(){
+                ctlr.display = ctlr.collection;
+            }
+
+            // Populate the list of songs
+            ctlr.loadLibrary = function(){
+                httpLoader.load('http://localhost:3000/api/mycollection', function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        ctlr.collection = result;
+                        ctlr.displaySongs();
+                    }
+                });
+            }
+
+            // Populate the list of playlists
+            ctlr.loadPlaylists = function(){
+                httpLoader.load('http://localhost:3000/api/myplaylists', function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        ctlr.playlists = result;
+                    }
+                });
+            }
+
+            ctlr.loadLibrary();
+            ctlr.loadPlaylists();
+
+            $('.playlistForm').hide();
+            $('.addPlaylist').click(function(){
+                $('.playlistForm').show();
+            });
+
+        }],
+        controllerAs: "ctlr"
+    };
+}]);
