2a0e3e25827434c0fce8c66bdde1d1412c05c187
mimen <miladmaaan@gmail.com>
2016-05-07 21:33:12 -0700

    rewrote collection add to use soundcloud's live API in order to get playlist information

--- client/database/collections.js
+++ client/database/collections.js
@@ -8,117 +8,210 @@ module.exports = function(db){
     // tracks to the database that do not yet exist.
     module.addCollection = function(user, collection, done){
         console.log("adding collection to database");
-        addTracks(user, collection, 0, done);
+        addItems(user, collection, 0, done);
     };
 
     // Recursive helper function for addCollection.
-    function addTracks(user, collection, index, done){
-        track = collection[index];
-        if (track.kind == 'track'){
-            
-            module.checkExistence(user.properties.scuid, track, function(found, error){
-                if (error)
-                    done(error);
-                //user to track relationship already exists in database; done.
-                if (found) 
-                    done();
-                //otherwise relationship does not yet exist; create it.
-                else {
-                    var query = 'MATCH (u:Channel {name: {name}}) ' + 
-                                'MERGE (t:Track { name: {title}, duration: {duration}, scid: {tid}, ' +
-                                'url: {url}, tag_list: {tag_list}, created_at: {created_at}, ';
-                    if(track.genre != null)
-                        query += 'genre: {genre}, ';
-                    if (track.purchase_url != null)
-                        query += 'purchase_url: {purchase_url}, ';
-                    if (track.artwork_url != null)
-                        query += 'artwork_url: {artwork_url}, ';
-                    query = query + 'waveform_url: {waveform_url} }) ' +
-                                'MERGE (c:Channel { name: {channel} }) ' + 
-                                'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
-                                'CREATE (u)-[r1:LIKES_TRACK]->(t) ' +
-                                'CREATE (c)-[r2:UPLOADED]->(t) ' +
-                                'RETURN u, r1, c, r2, t';
-
-                    db.cypher({ 
-                        query: query,
-                        params: {
-                            name: user.properties.name,
-                            title: track.title,
-                            duration: track.duration,
-                            genre: track.genre,
-                            tid: track.id,
-                            url: track.permalink_url,
-                            purchase_url: track.purchase_url,
-                            tag_list: track.tag_list,
-                            artwork_url: track.artwork_url,
-                            created_at: track.created_at,
-                            waveform_url: track.waveform_url,
-                            channel: track.user.username,
-                            channel_url: track.user.permalink_url,
-                            avatar_url: track.user.avatar_url,
-                            uid: track.user.id
-                        },
-                    }, function(error, results){
-                        if (error){
-                            done(error);
-                        }
-                        else {
+    function addItems(user, collection, index, done){
+        item = collection[index];
+        
+        module.checkExistence(user.properties.scuid, item, function(found, error){
+            if (error)
+                done(error);
+            //user to item relationship already exists in database; done.
+            if (found) 
+                done();
+            //otherwise relationship does not yet exist; create it.
+            else {
+                if (item.track){
+                    addTrack(user, item.track, function(success, error){
+                        if (success){
                             console.log("Track added!");
                             index++;
                             if (index < collection.length - 1) 
-                                addTracks(user, collection, index, done);
+                                addItems(user, collection, index, done);
                             else
                                 done();
                         }
+                        else {
+                            done(error);
+                        }
                     });
-
+                } 
+                else {
+                    addPlaylist(user, item.playlist, function(success, error){
+                        if (success){
+                            console.log("Playlist added!");
+                            index++;
+                            if (index < collection.length - 1) 
+                                addItems(user, collection, index, done);
+                            else
+                                done();
+                        }
+                        else {
+                            done(error);
+                        }
+                    });
                 }
+            }
+        })
 
-            })
 
-        } 
-        else {
-            console.log("attempted to add playlist");
-            //TODO: Add tracks from this playlist 
-            index++;
-            if (index < collection.length - 1) 
-                addTracks(user, collection, index, done);
-            else
-                done();
-        }
+        
+    }
+
+    function addTrack(user, track, done){
+
+        var query = 'MATCH (u:Channel {name: {name}}) ' + 
+                    'MERGE (t:Track { name: {title}, duration: {duration}, scid: {tid}, ' +
+                    'url: {url}, tag_list: {tag_list}, created_at: {created_at}, ';
+        if (track.genre != null)
+            query += 'genre: {genre}, ';
+        if (track.purchase_url != null)
+            query += 'purchase_url: {purchase_url}, ';
+        if (track.artwork_url != null)
+            query += 'artwork_url: {artwork_url}, ';
+        query = query + 'waveform_url: {waveform_url} }) ' +
+                    'MERGE (c:Channel { name: {channel} }) ' + 
+                    'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
+                    'CREATE (u)-[r1:LIKES_TRACK]->(t) ' +
+                    'CREATE (c)-[r2:UPLOADED]->(t) ' +
+                    'RETURN u, r1, c, r2, t';
+
+        db.cypher({ 
+            query: query,
+            params: {
+                name: user.properties.name,
+                title: track.title,
+                duration: track.duration,
+                genre: track.genre,
+                tid: track.id,
+                url: track.permalink_url,
+                purchase_url: track.purchase_url,
+                tag_list: track.tag_list,
+                artwork_url: track.artwork_url,
+                created_at: track.created_at,
+                waveform_url: track.waveform_url,
+                channel: track.user.username,
+                channel_url: track.user.permalink_url,
+                avatar_url: track.user.avatar_url,
+                uid: track.user.id
+            },
+        }, function(error, results){
+            if (error){
+                done(false, error);
+            }
+            else {
+                done(true);
+            }
+        });
+
+    }
+
+
+    function addPlaylist(user, playlist, done){
+
+        var query = 'MATCH (u:Channel {name: {name}}) ' + 
+                    'MERGE (p:SCPlaylist { name: {title}, scid: {scid}, ';
+        if (playlist.purchase_url != null)
+            query += 'purchase_url: {purchase_url}, ';
+        if (playlist.artwork_url != null)
+            query += 'artwork_url: {artwork_url}, ';
+        query = query + 'url: {url}, created_at: {created_at} }) ' +
+                    'MERGE (c:Channel { name: {channel} }) ' + 
+                    'ON MATCH SET c.channel_url = {channel_url}, c.avatar_url = {avatar_url}, c.scid = {uid} ' + 
+                    'CREATE (u)-[r1:LIKES_PLAYLIST]->(p) ' +
+                    'CREATE (c)-[r2:UPLOADED]->(p) ' +
+                    'RETURN u, r1, c, r2, p';
+
+        db.cypher({ 
+            query: query,
+            params: {
+                name: user.properties.name,
+                title: playlist.title,
+                scid: playlist.id,
+                url: playlist.permalink_url,
+                purchase_url: playlist.purchase_url,
+                artwork_url: playlist.artwork_url,
+                created_at: playlist.created_at,
+                channel: playlist.user.username,
+                channel_url: playlist.user.permalink_url,
+                avatar_url: playlist.user.avatar_url,
+                uid: playlist.user_id
+            },
+        }, function(error, results){
+            if (error){
+                done(false, error);
+            }
+            else {
+                done(true);
+            }
+        });
+
     }
 
 
     // Check for the existence of a [:LIKES] relationship between a channel and a track. 
     // Return true if it exists, false if it does not. 
-    module.checkExistence = function(scuid, track, done){
+    module.checkExistence = function(scuid, item, done){
 
-        db.cypher({ 
-            query: 'MATCH (u:Channel {scuid: {scuid} }), (t:Track {scid: {scid} } ), (u)-[:LIKES_TRACK]->(t) return u, t',
-            params: {
-                scuid: scuid,
-                scid: track.id
-            },
+        if (item.track){
+            db.cypher({ 
+                query: 'MATCH (u:Channel {scuid: {scuid} }), (t:Track {scid: {scid} } ), (u)-[:LIKES_TRACK]->(t) return u, t',
+                params: {
+                    scuid: scuid,
+                    scid: item.track.id
+                },
 
-        }, function(error, results){
-            if (error){
-                done(null, error);
-            }
-            else {      
-                //console.log(results);
-                // If no match, create an entry for the user
-                if (results.length == 0) {
-                    console.log("No relationship found.");
-                    done(false);
-                    //TODO: Update access token
+            }, function(error, results){
+                if (error){
+                    done(null, error);
                 }
-                else {
-                    console.log("Relationship already exists.");
-                    done(true);
+                else {      
+                    //console.log(results);
+                    // If no match, create an entry for the user
+                    if (results.length == 0) {
+                        console.log("No relationship found.");
+                        done(false);
+                        //TODO: Update access token
+                    }
+                    else {
+                        console.log("Relationship already exists.");
+                        done(true);
+                    }
                 }
-            }
-        });
+            });
+        }
+        else
+        if (item.playlist){
+            db.cypher({ 
+                query: 'MATCH (u:Channel {scuid: {scuid} }), (p:SCPlaylist {scid: {scid} } ), (u)-[:LIKES_PLAYLIST]->(p) return u, p',
+                params: {
+                    scuid: scuid,
+                    scid: item.playlist.id
+                },
+
+            }, function(error, results){
+                if (error){
+                    done(null, error);
+                }
+                else {      
+                    //console.log(results);
+                    // If no match, create an entry for the user
+                    if (results.length == 0) {
+                        console.log("No relationship found.");
+                        done(false);
+                        //TODO: Update access token
+                    }
+                    else {
+                        console.log("Relationship already exists.");
+                        done(true);
+                    }
+                }
+            });
+        }
+
+        
     };
 
     // Given a user, find and return their entire collection of songs, along with the channels
