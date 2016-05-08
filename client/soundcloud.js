7f4f355c5512fb1ce41beb7b6ab8065d35a2c6cb
Milad Imen <mimen@ucsd.edu>
2016-05-07 21:33:12 -0700

    rewrote collection add to use soundcloud's live API in order to get playlist information

--- client/soundcloud.js
+++ client/soundcloud.js
@@ -43,17 +43,25 @@ function getLoggedInUser(accessToken, done){
 
 //get a user's collection 
 function getCollection(user, done){
-    var href = 'https://api.soundcloud.com/users/' + user.id 
-        + '/favorites?client_id=' + config.auth.client_id + '&linked_partitioning=1&limit=200';
+
+    // var href = 'https://api.soundcloud.com/users/' + user.id 
+    //     + '/favorites?client_id=' + config.auth.client_id + '&linked_partitioning=1&limit=200';
+
+    //TODO: Use this API url (gives more results, and includes playlists)
+    var href = 'https://api-v2.soundcloud.com/users/' + user.id 
+        + '/likes?client_id=' + config.auth.client_id + '&linked_partitioning=1&limit=200';
+
+
     getCollectionRecurse(user, [], href, done);
 }
 
 function getCollectionRecurse(user, collection, next_href, done){
+    console.log("here");
+        console.log(next_href);
     getRequest(next_href, function(response){
-        console.log(response.next_href);
         var updatedCollection = collection.concat(response.collection);
-        if (response.next_href){ 
-            var href = response.next_href;
+        if (response.next_href && collection.length < 500){ 
+            var href = response.next_href + '&client_id=' + config.auth.client_id;
             database.checkExistence(user.id, response.collection[0], function(found, error){
                 if (error)
                     done(null, error);
