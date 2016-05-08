f184adb9cda4c6d06d2e6a6ef3cc16380d479b6c
mimen <miladmaaan@gmail.com>
2016-05-08 15:00:03 -0700

    adding support for retreiving channels from database

---
+++ client/database/channels.js
@@ -0,0 +1,61 @@
+module.exports = function(db){
+
+    var module = {};
+
+    module.getChannels = function(uid, done){
+        db.cypher({ 
+            query: 'MATCH (u:Channel)-[:LIKES_TRACK]->(Track)<-[:UPLOADED]-(c:Channel) ' + 
+                   'WHERE id(u) = {uid} ' +
+                   'RETURN c',
+            params: {
+                uid: uid
+            }
+        }, function(error, results){
+            if (error){
+                done(null, error);
+            }
+            else {      
+                // If no match, create an entry for the user
+                if (results.length == 0) {
+                    console.log("No user found.");
+                }
+                else {
+                    console.log("User was found.");
+                }
+                done(results);
+            }
+        });
+    }
+
+    module.getLikedTracksByChannel = function(uid, cid, done){
+        db.cypher({ 
+            query: 'MATCH (u:Channel)-[:LIKES_TRACK]->(t:Track)<-[:UPLOADED]-(c:Channel) ' + 
+                   'WHERE id(u) = {uid} ' +
+                   'AND id(c) = {cid} ' +
+                   'RETURN t',
+            params: {
+                uid: uid,
+                cid: cid
+            }
+        }, function(error, results){
+            if (error){
+                done(null, error);
+            }
+            else {      
+                // If no match, create an entry for the user
+                if (results.length == 0) {
+                    console.log("No user found.");
+                }
+                else {
+                    console.log("User was found.");
+                }
+                done(results);
+            }
+        });
+    }
+
+
+
+    return module;
+
+}
