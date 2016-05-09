971efe8c74b056a0079443915250dfafcae759a3
Milad Imen <mimen@ucsd.edu>
2016-05-09 00:22:15 -0700

    removed unused api endpoints

--- routes/api.js
+++ routes/api.js
@@ -12,47 +12,7 @@ router.use('/scplaylists/', scplaylists);
 
 /* GET API index. */
 router.get('/', function(req, res, next) {
-	res.json({ error: 'There is nothing here.' });
+	res.json({ error: 'Use endpoints /users/ /playlists/ and /scplaylists/.' });
 });
-
-/* GET logged in user's collection. */
-router.get('/mycollection/', function(req, res, next) {
-	// Retrieve the user data from the session.
-	var user = req.session.user;
-	// Get the collection from the database and render the json.
-	db.getCollection(user._id, function(collection){
-		res.json(collection);
-	});
-})
-
-/* GET logged in user's custom playlists. */
-router.get('/myplaylists/', function(req, res, next) {
-	// Retrieve the user data from the session.
-	var user = req.session.user;
-	// Get the collection from the database and render the json.
-	db.getPlaylists(user._id, function(playlists){
-		res.json(playlists);
-	});
-})
-
-/* GET logged in user's soundcloud playlists. */
-router.get('/myscplaylists/', function(req, res, next) {
-	// Retrieve the user data from the session.
-	var user = req.session.user;
-	// Get the collection from the database and render the json.
-	db.getSCPlaylists(user._id, function(playlists){
-		res.json(playlists);
-	});
-})
-
-/* GET logged in user's soundcloud channels. */
-router.get('/mychannels/', function(req, res, next) {
-	// Retrieve the user data from the session.
-	var user = req.session.user;
-	// Get the collection from the database and render the json.
-	db.getChannels(user._id, function(channels){
-		res.json(channels);
-	});
-})
 
 module.exports = router;
