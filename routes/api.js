f4576d325044ee01a514cfb963d8e5d562ed1feb
mimen <miladmaaan@gmail.com>
2016-05-11 16:46:32 -0700

    removed reference to sclibrary endpoint

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ var db = require('../client/database');
 
 var users = require('./api/users.js');
 var playlists = require('./api/playlists.js');
-var scplaylists = require('./api/scplaylists.js');
 
 router.use('/users/', users);
 router.use('/playlists/', playlists);
