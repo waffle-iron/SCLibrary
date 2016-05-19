49e8b28f46a91978c5e97527a6d694ed1bf781bb
mimen <miladmaaan@gmail.com>
2016-05-18 20:26:06 -0700

    added approve request endpoint to API

--- routes/api/requests.js
+++ routes/api/requests.js
@@ -12,4 +12,15 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/* GET Approve request. */
+router.get('/:id/approve', function(req, res, next) {
+	console.log(req.params.id);
+	db.approveRequest(req.params.id, function(accounts, error){
+		if (error)
+			res.json({"error":"failed"});
+		else
+			res.json(accounts);
+	});
+});
+
 module.exports = router;
