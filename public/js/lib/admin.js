f663ad5fd9dece0b668f39bea06096191b76f6c9
mimen <miladmaaan@gmail.com>
2016-05-18 18:21:12 -0700

    admin page using angular to get data from API

--- public/js/lib/admin.js
+++ public/js/lib/admin.js
@@ -10,6 +10,9 @@ app.directive("admin", [function (){
         link: {
             pre: function(scope, element, attr) {
 
+                // Load accounts and requests
+                scope.loadAccounts();
+                scope.loadRequests();
 
             },
             post: function(scope, element, attr) {
@@ -23,6 +26,26 @@ app.directive("admin", [function (){
 // Library controller
 app.controller("AdminCtlr", function($scope, $http){
 
+    // Populate the list of songs
+    $scope.loadAccounts = function(){
+        var url = 'http://localhost:3000/api/accounts';
+        $http.get(url).then(function(response){
+            console.log(response);
+            $scope.accounts = response.data;
+        }, function(error){
+            console.log(error);
+        });
+    }
 
+    // Populate the list of songs
+    $scope.loadRequests = function(){
+        var url = 'http://localhost:3000/api/requests';
+        $http.get(url).then(function(response){
+            console.log(response);
+            $scope.accounts = response.data;
+        }, function(error){
+            console.log(error);
+        });
+    }
 
 });
