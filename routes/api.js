6a4bc13a52997ee98f58b7f87d68a7d2f123066f
mimen <miladmaaan@gmail.com>
2016-06-04 23:40:40 -0700

    added waveform endpoint for tracks

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../client/database');
+var request = require('request');
 
 var users = require('./api/users.js');
 var playlists = require('./api/playlists.js');
@@ -17,6 +18,29 @@ router.use('/requests/', requiresAdmin, requests);
 /* GET API index. */
 router.get('/', function(req, res, next) {
 	res.json({ error: 'Use endpoints /users/ /playlists/ and /scplaylists/.' });
+});
+
+router.get('/track/waveform/:id', function(req, res, next) {
+	db.getTrack(req.params.id, function(track, error){
+		if (error)
+			res.json({"error":"failed"});
+		else{
+		    var options = {
+		        url: track[0].n.properties.waveform_url,
+		        method: 'GET'
+		    }
+		    request(options, function(error, message, object){
+		        if (error){
+		            res.json({"error":"failed"});
+		        }
+		        else {
+		            var json = JSON.parse(object);
+		            res.json(json.samples);
+		        }
+		    })
+
+		}
+	});
 });
 
 module.exports = router;
