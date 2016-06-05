b1eb5e372681c14c3e64eee1502950c67aa93f3f
Milad Imen <mimen@ucsd.edu>
2016-06-04 23:40:24 -0700

    added getTrack function to database client

--- client/database.js
+++ client/database.js
@@ -12,6 +12,21 @@ var playlists = require('./database/playlists.js')(db);
 var channels = require('./database/channels.js')(db);
 var accounts = require('./database/accounts.js')(db);
 
+var getTrack = function(id, done){
+    db.cypher({ 
+        query: 'MATCH (n:Track) ' + 
+               'WHERE id(n) = ' + parseInt(id) + ' ' + 
+               'RETURN n'
+    }, function(error, results){
+        if (error){
+            done(null, error);
+        }
+        else {
+            done(results);
+        }
+    });
+}
+
 module.exports = {
     instance: db,
     addUser: users.addUser,
@@ -37,5 +52,6 @@ module.exports = {
     getRequest: accounts.getRequest,
     approveAccount: accounts.approveAccount,
     denyAccount: accounts.denyAccount,
-    approveRequest: accounts.approveRequest
+    approveRequest: accounts.approveRequest,
+    getTrack: getTrack
 }     
