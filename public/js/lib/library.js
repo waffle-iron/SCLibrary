3dbb66f2c4b5715dc3c267a4e95768e4098ba8f2
mimen <miladmaaan@gmail.com>
2016-06-04 23:41:32 -0700

    added waveform to library page player

--- public/js/lib/library.js
+++ public/js/lib/library.js
@@ -229,7 +229,8 @@ app.controller("LibraryCtlr", function($scope, $http){
         var url = 'http://localhost:3000/api/users/' + uid + '/collection/';
         $http.get(url).then(function(response){
             console.log(response);
-            $scope.collection = response.data;
+            var faster = [response.data[0], response.data[1]];
+            $scope.collection = response.data.splice(0, 200);
             $scope.displaySongs();
         }, function(error){
             console.log(error);
@@ -385,3 +386,61 @@ app.controller("LibraryCtlr", function($scope, $http){
     }
 
 });
+
+
+app.directive('player', function ($parse) {
+     //explicitly creating a directive definition variable
+     //this may look verbose but is good for clarification purposes
+     //in real life you'd want to simply return the object {...}
+     var directiveDefinitionObject = {
+         //We restrict its use to an element
+         //as usually  <bars-chart> is semantically
+         //more understandable
+         restrict: 'E',
+         //this is important,
+         //we don't want to overwrite our directive declaration
+         //in the HTML mark-up
+         replace: false,
+         //our data source would be an array
+         //passed thru chart-data attribute
+         scope: {data1: '=chartData'},
+         link: function (scope, element, attrs) {
+           //in D3, any selection[0] contains the group
+           //selection[0][0] is the DOM node
+           //but we won't need that this time
+           //to our original directive markup bars-chart
+           //we add a div with out chart stling and bind each
+           //data entry to the chart
+
+              var height = 110;
+              var width = "100%";
+
+              var data = [];
+              var b = 10;
+              for (var i = 0; i < data1.length/b; i++){
+                data.push((data1[(i * b)] + data1[(i * b) + 1] + data1[(i * b) + 2] + data1[(i * b) + 3] + data1[(i * b) + 4] + data1[(i * b) + 5] + data1[(i * b) + 6] + data1[(i * b) + 7] + data1[(i * b) + 8] + data1[(i * b) + 9])/b);
+              }
+
+              console.log(data);
+
+              var w = 9, h = d3.max(data);
+
+              var chart = d3.select(element[0]).append("svg")
+                .attr("class", "chart")
+                .attr("width", "100%")
+                .attr("height", "100px")
+                .attr("fill", "white")
+                .attr("viewBox", "0 0 " + (w * data.length) + " " + h );;
+
+              var x = d3.scale.linear()
+                .domain([0, 1])
+                .range([0, w]);
+                 
+              var y = d3.scale.linear()
+                .domain([0, h])
+                .rangeRound([0, h]); //rangeRound is used for antialiasing
+
+         } 
+      };
+      return directiveDefinitionObject;
+   });
