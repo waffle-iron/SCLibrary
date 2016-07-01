b05eb0a9bd28b6a93a0f11a3004f0e695a8b52de
Milad Imen <mimen@mimen-ltm6.internal.salesforce.com>
2016-07-01 00:30:52 -0700

    cleaned up api call logic

--- routes/api/requests.js
+++ routes/api/requests.js
@@ -22,51 +22,48 @@ router.post('/:id/approve', function(req, res, next) {
 	db.getRequest(rid, function(request, error){
 		if (error)
 			res.json(error);
-		else{
+		else {
 			console.log(request);
 			var username = request[0].r.properties.username;
-
 			sc.getUserFromUsername(username, function(sc_user, error){
 				if (error) {
 					res.json(error);
 				}
 				db.addUser(sc_user, function(db_user, error){
-          			if (error) {
-            			res.json(error);
-          			}
-          			// Retrieve the user's favorites from the Soundcloud API.
-      				sc.getCollection(sc_user, function(collection, error){
-        				if (error){
-          					res.json(error);
-        				}
-        				// Add the user's collection to the database.
-        				db.addCollection(db_user, collection, function(error, pids){
-          					if (error) {
-           						res.json(error);
-          					}
-      						sc.getPlaylists(pids, function(playlists, error){
+    			if (error) {
+      			res.json(error);
+    			}
+  				sc.getCollection(sc_user, function(collection, error){
+    				if (error){
+      					res.json(error);
+    				}
+    				db.addCollection(db_user, collection, function(error, pids){
+    					if (error) {
+     						res.json(error);
+    					}
+  						sc.getPlaylists(pids, function(playlists, error){
+  							if (error) {
+       						res.json(error);
+      					}
+  							db.addPlaylistTracks(playlists, function(complete, error){
+    							if (error) {
+       							res.json(error);
+    							}
+      						var uid = db_user._id;
+      						db.approveRequest(aid, rid, uid, function(request, error){
       							if (error) {
-	           						res.json(error);
-	          					}
-        						db.addPlaylistTracks(playlists, function(complete, error){
-	      							if (error) {
-	           							res.json(error);
-          							}
-              						var uid = db_user._id;
-              						db.approveRequest(aid, rid, uid, function(request, error){
-              							if (error) {
-              								res.json(error);
-              							}
-              							else {
-              								res.json(request);
-              							}
-              						})
-            					});
-          					});
-        				});
-      				});
-        		});
-			})
+      								res.json(error);
+      							}
+      							else {
+      								res.json(request);
+      							}
+      						});
+      					});
+    					});
+  					});
+  				});
+    		});
+			});
 		}
 	});
 });
