4ac4f703d3029c6822030b8d011778881777c330
kmannislands <kmann@ucsd.edu>
2016-07-06 22:02:17 -0700

    Reinstate the modal
    Self-explanatory

--- public/js/lib/library.js
+++ public/js/lib/library.js
@@ -229,7 +229,7 @@ app.controller("LibraryCtlr", function($scope, $http){
         var url = 'http://localhost:3000/api/users/' + uid + '/collection/';
         $http.get(url).then(function(response){
             console.log(response);
-            $scope.collection = response.data.splice(3800, 4000);
+            $scope.collection = response.data;//.splice(3800, 4000);
             $scope.displaySongs();
         }, function(error){
             console.log(error);
